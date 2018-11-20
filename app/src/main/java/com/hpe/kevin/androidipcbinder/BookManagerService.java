8b1f453f81b8cf48e73f523c4887bd80c1fc39e8
Kevin <momocha0625@126.com>
2018-11-20 16:06:02 +0800

    服务端程序

---
+++ app/src/main/java/com/hpe/kevin/androidipcbinder/BookManagerService.java
@@ -0,0 +1,45 @@
+package com.hpe.kevin.androidipcbinder;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManagerService extends Service {
+    private ArrayList<Book> mBookList = new ArrayList<Book>();
+    private Binder mBinder = new IBookManager.Stub() {
+        @Override
+        public List<Book> getBookList() throws RemoteException {
+            return mBookList;
+        }
+
+        @Override
+        public void addBook(Book book) throws RemoteException {
+            mBookList.add(book);
+        }
+    };
+
+    public BookManagerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    /**
+     * Called by the system when the service is first created.  Do not call this method directly.
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e("TAG", "---->远程服务启动");
+        mBookList.add(new Book(1, "book1"));
+        mBookList.add(new Book(2, "book2"));
+    }
+}
